46c0ecb5e301b3772d3b2d2fa82257f1f51622e3
20522182 <205222182@gm.uit.edu.vn>
2023-07-06 16:02:31 +0700

    Update admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,35 +1,33 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
-admin.site.register (Book)
-admin.site.register (Author)
+
+
 admin.site.register (Genre)
-admin.site.register (BookInstance)
+
 # admin.site.register(Author)
 # Define the admin class
 # Define the admin class
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-
 # Register the admin class with the associated model
-#python3 manage.py runserver
-#admin.site.register(Author, AuthorAdmin)
-# admin.site.register(Book)
-# admin.site.register(BookInstance)
+admin.site.register(Author, AuthorAdmin)
 # Register the Admin classes for Book using the decorator
-#@admin.register(Book)
+# Register the Admin classes for Book using the decorator
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
     inlines = [BooksInstanceInline]
-# Register the Admin classes for BookInstance using the decorator
-#@admin.register(BookInstance)
 
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
 
@@ -41,4 +39,3 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
-
