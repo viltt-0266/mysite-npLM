e5220a895052d5b9c4db8cfcb020b754e1d96b76
20522182 <205222182@gm.uit.edu.vn>
2023-07-10 12:50:12 +0700

    Django Tutorial Part 6: Generic list and detail views

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
-    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('allbooks/', views.LoanedBooksByStaffListView.as_view(), name='all-borrowed'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail')
 ]
-
 
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views import generic
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 def index(request):
     """View function for home page of site."""
@@ -15,18 +14,15 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
     # The 'all()' is implied by default.
-    num_authors = Author.objects.count()  
-    # Number of visits to this view, as counted in the session variable.
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+    num_authors = Author.objects.count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_visits': num_visits,
     }
+
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
@@ -42,29 +38,3 @@ class AuthorListView(generic.ListView):
     
 class AuthorDetailView(generic.DetailView):
     model = Author
-
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
-    """Generic class-based view listing books on loan to current user."""
-    model = BookInstance
-    template_name = 'catalog/bookinstance_list_borrowed_user.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        return (
-            BookInstance.objects.filter(borrower=self.request.user)
-            .filter(status__exact='o')
-            .order_by('due_back')
-        )
-
-class LoanedBooksByStaffListView(LoginRequiredMixin,generic.ListView):
-    """Generic class-based view listing books on loan to current user."""
-    model = BookInstance
-    template_name = 'catalog/bookinstance_list_borrowed_staff.html'
-    paginate_by = 10
-
-    def get_queryset(self):
-        return (
-            BookInstance.objects.filter()
-            .filter(status__exact='o')
-            .order_by('due_back')
-        )
